0e66009767ce93e15b95615dbd2eb129f5e985d7
Bob Anzlovar <rcanzlovar@gmail.com>
2019-09-13 13:40:43 -0600

    finished with for loops

--- learnpythoninonedayandwell/forwhile.py
+++ learnpythoninonedayandwell/forwhile.py
@@ -3,8 +3,7 @@
 # for loopo
 print("=0=-"*12)
 
-# while loop
-if (3 == 3):
+if (3 == 2):
     userInput = input('Enter 1 or 2: ')
     if userInput=="1":
         print ("Hello World")
@@ -17,18 +16,29 @@ if (3 == 3):
     else:
         print ("you did not enter a valid number")
 
-# for iterates over an iterable
-iterthing = [1, 2, 3, 4, 5]
-for a in iterthing:
-	print (str(a) + " thing")
+if ('for' == 'in'):
+    # for iterates over an iterable
+    iterthing = [1, 2, 3, 4, 5]
+    for a in iterthing:
+    	print (str(a) + " thing")
+    #
+    for a in [1, 2, 3, 4, 5]: print (str(a) + " thoing")
+    #
+    myDict = {'Bob':57, 'Ran':29}
+    for i,j in myDict.items(): 
+        print ("%s is %d\n" % (i,j))
+    for i in myDict:
+        print ("name %s age %d" % (i,myDict[i]))
+    #
+    for i in "Bob Anzlovar":
+        print (i)
+    # range (start, end, step)
+    # END is always excluded from result
+    for i in  range(5):
+        print (i)
+    #
+    for i in  range(3, 10+1):
+        print (i)
 
-for a in [1, 2, 3, 4, 5]: print (str(a) + " thoing")
 
-myDict = {'Bob':57, 'Ran':29}
-for i,j in myDict.items(): 
-    print ("%s is %d\n" % (i,j))
-for i in myDict:
-    print ("name %s age %d" % (i,myDict[i]))
-
-for i in "Bob Anzlovar":
-    print (i)
+# while loop
