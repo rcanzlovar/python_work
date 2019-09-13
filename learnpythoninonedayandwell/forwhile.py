be2d6a1a2504b61875ec0645a05e4bd38ac74b56
Bob Anzlovar <rcanzlovar@gmail.com>
2019-09-13 13:25:23 -0600

    more

--- learnpythoninonedayandwell/forwhile.py
+++ learnpythoninonedayandwell/forwhile.py
@@ -17,11 +17,18 @@ if (3 == 3):
     else:
         print ("you did not enter a valid number")
 
-
+# for iterates over an iterable
 iterthing = [1, 2, 3, 4, 5]
 for a in iterthing:
 	print (str(a) + " thing")
 
+for a in [1, 2, 3, 4, 5]: print (str(a) + " thoing")
+
 myDict = {'Bob':57, 'Ran':29}
-for i,j in myDict:
+for i,j in myDict.items(): 
     print ("%s is %d\n" % (i,j))
+for i in myDict:
+    print ("name %s age %d" % (i,myDict[i]))
+
+for i in "Bob Anzlovar":
+    print (i)
