7ad92e6b1f0aebdd6998c6560c6aab327319a9f5
Bob Anzlovar <rcanzlovar@gmail.com>
2019-09-14 13:13:46 -0600

    More modules

--- learnpythoninonedayandwell/mods.py
+++ learnpythoninonedayandwell/mods.py
@@ -2,7 +2,6 @@
 # keep the shebang for now. If i dont have it, i have to say 
 # python3 mods.py
 import math
-import random
 
 
 
@@ -10,5 +9,13 @@ newstring = "Hello moles".replace("moles", "Miles")
 print(newstring)
 
 #  Modules & Methods
-print (random.randrange(1, 10))
+## whole module
+#import random 
+#print (random.randrange(1, 10))
+## a little lazy... 
+#import random as r
+#print (r.randrange(1, 10))
+## just one bit
+from random  import randrange
+print (randrange(1, 10))
 
