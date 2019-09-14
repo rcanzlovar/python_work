5e5c2a561fb2f204e397d692e4cf2f95ed8132c6
Bob Anzlovar <rcanzlovar@gmail.com>
2019-09-14 12:57:59 -0600

    modules and methods....

---
+++ learnpythoninonedayandwell/mods.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# keep the shebang for now. If i dont have it, i have to say 
+# python3 mods.py
+import math
+import random
+
+
+
+newstring = "Hello moles".replace("moles", "Miles")
+print(newstring)
+
+#  Modules & Methods
+print (random.randrange(1, 10))
+
