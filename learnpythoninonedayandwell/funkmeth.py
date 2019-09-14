4e83f4dfbcde311ad98dddfcf485fcfaf3657bf1
Bob Anzlovar <rcanzlovar@gmail.com>
2019-09-14 12:41:17 -0600

    finished with Functions...

--- learnpythoninonedayandwell/funkmeth.py
+++ learnpythoninonedayandwell/funkmeth.py
@@ -6,12 +6,10 @@ import math
 newstring = "Hello world".replace("world", "universe")
 print(newstring)
 
-
 def checkIfPrime(numToCheck):
     top = int(math.sqrt(numToCheck)) + 1
     print(top)
     for x in range(2,top):
-        print(x) 
         if (numToCheck% x == 0):
             return False
         return True
@@ -26,8 +24,6 @@ def myfunction(value):
         print ("not prime") 
         return ("not prime") 
 
-print('x',myfunction(13))
-print('y',checkIfPrime(13))
 
 def addNumbers(*num):
     sum = 0
@@ -36,7 +32,30 @@ def addNumbers(*num):
     print (sum)
     return(sum)
 
+def printMembers (**age):
+    for i,j in age.items():
+        print("name=%s age=%s" 
+            % (i,j))
 
-print (addNumbers(1, 2, 3, 5))
-# modules
-# methods
+# mixing fixed args, vriable args and a dictionary
+# must be in this order 
+def someFunc(farg, *args, **kwargs):
+    print ("farg ",farg)
+    for i in args:
+        print ("args ",i)
+    for i,j in kwargs.items():
+        print("name=%s age=%s" 
+            % (i,j))
+    
+if (2 == 4):
+    print('x',myfunction(13))
+    print('y',checkIfPrime(13))
+
+    print (addNumbers(1, 2, 3, 5))
+
+    printMembers(dave=7)
+#    printMembers(bob = 57, ran = 29)
+
+someFunc("whee",2,3,5,bob=57,ran=29)
+
+
