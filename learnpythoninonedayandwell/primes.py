26faf059f9386a3145385fedd4d3781cf9b6a282
Bob Anzlovar <rcanzlovar@gmail.com>
2019-09-14 13:45:39 -0600

    let's nail the None thing

--- learnpythoninonedayandwell/primes.py
+++ learnpythoninonedayandwell/primes.py
@@ -4,11 +4,11 @@ import math
 def checkIfPrime(numToCheck):
     # check all of them
     top = numToCheck
-    # how about half?
-    top = int(numToCheck / 2) 
+    # how about half -ish?
+    top = int(numToCheck / 2) +1 
     # technically we just need to go up to square root
-    #top = int(math.sqrt(numToCheck)) + 1
-    print("top limit = ",top)
+    top = int(math.sqrt(numToCheck)) + 1
+    print("top limit for %s = %s" %(numToCheck, top))
     for x in range(2,top):
         if (numToCheck% x == 0):
             return False
--- learnpythoninonedayandwell/useCheckIfPrime.py
+++ learnpythoninonedayandwell/useCheckIfPrime.py
@@ -5,6 +5,7 @@ from random  import randrange
 
 # swich it up with a random number 
 num =  randrange(1, 20)
+num=3
 
 answer = primes.checkIfPrime(num)
 print ("answer for %d is %s" % (num,answer))
