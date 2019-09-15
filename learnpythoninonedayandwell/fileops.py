82990a0a9545731940af61f0947a81111697b867
Bob Anzlovar <rcanzlovar@gmail.com>
2019-09-14 19:00:25 -0600

    file reads and appends

--- learnpythoninonedayandwell/fileops.py
+++ learnpythoninonedayandwell/fileops.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
 
+# read two lines, print them
 f = open('myfile.txt','r')
 
 firstline = f.readline()
 secondline = f.readline()
-print (firstline)
+print (firstline, end='TOTALLY THE END')
 print (secondline)
 
+f.close()
+
+# read through the file 
+f = open('myfile.txt','r')
+for line in f:
+    print (line, end='')
+f.close()
+
+# add a couple of lines
+f = open('myfile.txt','a')
+f.write('\nThis senencei will append')
+f.write('\nPython is fun')
 f.close()
